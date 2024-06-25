20abae4a71dbcaca7e03e4e732e168f9aa8b218c
Ilya Seleznev <zedplay.zp@gmail.com>
2024-06-25 20:38:35 +0300

    Сделал задание

--- double_linked_list.py
+++ double_linked_list.py
@@ -52,4 +52,101 @@ class LinkedList:
         return "Список выведен с начала"
 
 
+class UpdatedLL(LinkedList):
+    def __init__(self):
+        super().__init__()
 
+    def print_ll_from_tail(self):
+        current_node = self.tail
+        while current_node is not None:
+            print(current_node.data)
+            current_node = current_node.prev_node
+        return "Список выведен с конца"
+
+    def len_ll(self):
+        current_node = self.head
+        length = 0
+        while current_node is not None:
+            length += 1
+            current_node = current_node.next_node
+        return length
+
+    def insert_at_index(self, data, index):
+        new_node = Node(data)
+        ll_length = self.len_ll()
+        if ll_length >= index:
+            if index == 0:
+                self.insert_at_head(data)
+            else:
+                current_index = 0
+                current_node = self.head
+                while current_node is not None:
+                    if index == current_index:
+                        new_node.prev_node = current_node.prev_node
+                        current_node.prev_node.next_node = new_node
+                        current_node.prev_node = new_node
+                        new_node.next_node = current_node
+                        return "Done"
+                    current_index += 1
+                    current_node = current_node.next_node
+        else:
+            return "Out of range"
+
+    def remove_node_index(self, index):
+        ll_length = self.len_ll()
+        if ll_length >= index:
+            if index == 0:
+                self.remove_from_head()
+            elif index == ll_length - 1:
+                self.remove_from_tail()
+            else:
+                current_index = 0
+                current_node = self.head
+                while current_node is not None:
+                    if index == current_index:
+                        current_node.prev_node.next_node = current_node.next_node
+                        current_node.next_node.prev_node = current_node.prev_node
+                        return "Done"
+                    current_index += 1
+                    current_node = current_node.next_node
+        else:
+            return "Out of range"
+
+    def __contains_from_head(self, data):
+        current_node = self.head
+        while current_node is not None:
+            if current_node.data == data:
+                return True
+            else:
+                current_node = current_node.next_node
+        return False
+
+    def __contains_from_tail(self, data):
+        current_node = self.tail
+        while current_node is not None:
+            if current_node.data == data:
+                return True
+            else:
+                current_node = current_node.prev_node
+        return False
+
+    def contains_from(self, data, method="head"):
+        if method == "head":
+            return self.__contains_from_head(data)
+        elif method == "tail":
+            return self.__contains_from_tail(data)
+        else:
+            return 'Метод должен быть либо "head", либо "tail"'
+
+    def remove_node_data(self, data):
+        if self.contains_from(data):
+            current_index = 0
+            current_node = self.head
+            while current_node is not None:
+                if current_node.data == data:
+                    self.remove_node_index(current_index)
+                else:
+                    current_index += 1
+                    current_node = current_node.next_node
+        else:
+            return 'data не найдена'
